71801bd2d9f51d3a300ec7b53fee29c3b177b3b9
Bunyod <bunyod.kh@gmail.com>
2026-05-15 21:18:41 +0500

    upd settings

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
